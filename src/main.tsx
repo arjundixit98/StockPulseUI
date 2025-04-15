d0af716ef2aa2b5863445ce302c0dbab218fa90c
Arjun Dixit <arjun.dixit98@gmail.com>
2025-04-15 22:26:08 +0530

    cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ScreenerFrame } from './components/portfolio/ScreenerFrame.tsx';
 createRoot(document.getElementById("root")!).render(<App />);
 
 //createRoot(document.getElementById("root")!).render(<ScreenerFrame />);
